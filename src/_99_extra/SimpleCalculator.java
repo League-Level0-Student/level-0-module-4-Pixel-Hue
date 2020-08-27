d1b6c2b15ce1bf9eb5019c7c02c5370ff530482c
root <root@0afc53771606>
2020-08-27 23:55:26 +0000

    Simple Calculator

--- src/_99_extra/SimpleCalculator.java
+++ src/_99_extra/SimpleCalculator.java
@@ -7,20 +7,57 @@ public class SimpleCalculator {
 	public static void main(String[] args) {
 
 		// 1. Get 2 numbers from the user and convert them to integer.
-
+String a = JOptionPane.showInputDialog("Write a number");
+int b = Integer.parseInt(a);
+String c = JOptionPane.showInputDialog("Write another number");
+int d = Integer.parseInt(a);
 		// 2. Customize pop-up to support add/subtract/multiply/divide operations.
-		int operation = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Button1", "Button2", "Button3", "Button3" },
+		int operation = JOptionPane.showOptionDialog(null, "Which math term would you like to use?", "Pop-up Title", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "+ (Plus)", "- (Minus)", "x (Multiplication)", "÷ (Division)" },
 				null);
 
 		// 5. Call the methods created in steps 3 and 4 to perform the appropriate operation.
-
+		//Use int operation to call the methods
+			plus(b, d);
+			minus(b, d);
+			multiplication(b, d);
+			division(b, d);
+		
+		
 		// 6. Wrap steps 1, 2 and 5 in a for loop to repeat calculator operations
 	}
-
+	
 	// 3. Create method for addition operation.
 	//    Method should create pop-up that shows equation and solution, eg. 45 + 25 = 70.
 	//    HINT: use 'static void add(int num1, num2) { ... }
-
+static void plus(int b, int d) {
+	int aa = 0;
+	aa+=b;
+	aa+=d;
+	
+	JOptionPane.showMessageDialog(null, "" + b + " + " + d + " = " + aa);
+}
+static void minus(int b, int d) {
+	int aa = 0;
+	aa+=b;
+	aa-=d;
+	
+	JOptionPane.showMessageDialog(null, "" + b + " - " + d + " = " + aa);
+}
+static void multiplication(int b, int d) {
+	int aa = 0;
+	aa+=b;
+	aa*=d;
+	
+	JOptionPane.showMessageDialog(null, "" + b + " x " + d + " = " + aa);
+}
+static void division(int b, int d) {
+	int aa = 0;
+	aa+=b;
+	aa/=d;
+	
+	JOptionPane.showMessageDialog(null, "" + b + " ÷ " + d + " = " + aa);
+}
 	// 4. Create similar methods for subtraction, multiplication and division.
+	
 }
--- src/_99_extra/StringMethods2.java
+++ src/_99_extra/StringMethods2.java
@@ -6,18 +6,34 @@ public class StringMethods2 {
 		
 		// 1. Create a String variable and initialize it with
 		//    random upper and lower case characters.
-
+String a = "AoIwqBNm";
 		// 2. Print your String to the console in upper case.
-		
+	
+		System.out.println(a.toLowerCase());
 		// 3. Print your String to the console in lower case.
-
+		
+		System.out.println(a.toUpperCase());
 		// 4. Print the first 3 char's of your String
 		//    HINT: .substring(start,end)
 		
+		System.out.println(a.substring(0, 3));
 		// 5. Print a single char somewhere in the middle of your String
 		//    REMINDER: char's in string start at index 0
-		
+		System.out.println(a.charAt(4));
 		// 6. BONUS -- print the LAST 3 char's of your string using
 		//        .length() to determine WHERE the last 3 char's are located.
+		System.out.println(a.length());
+		
+		
+		int c = a.length();
+		c-=3;
+	 
+		System.out.println(a.charAt(c));
+		c+=1;
+		System.out.println(a.charAt(c));
+		c+=1;
+		System.out.println(a.charAt(c));
+		
+		
 	}
 }
